185f1b51b196e28bfde59e06496de7e2ef44cc80
Jim Pudar <jmp@archimedes.local>
2020-04-15 19:19:40 -0400

    Add photos plugin and dependencies

--- pelicanconf.py
+++ pelicanconf.py
@@ -34,3 +34,6 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
+
+PLUGIN_PATHS = ["../pelican-plugins"]
+PLUGINS = ["photos"]
